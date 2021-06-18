df5330acbe60b745e5c6991d5af9cf92c7a8ce2c
Shevaister <degrae000@gmail.com>
2021-06-18 10:39:59 +0300

    1.3

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import(
 	"net/http"
 	"io"
 	"os"
+	"strconv"
 )
 
-func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts") 
+func flux(a string) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/" + a) 
 	if err != nil {
-		fmt.Print(nil)
+		fmt.Print(err)
 		return
 	}
-	defer resp.Body.Close() 
-   	io.Copy(os.Stdout, resp.Body)
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+	fmt.Println()
+}
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		go flux(strconv.Itoa(i))
+	}
+	var input string
+	fmt.Scanln(&input)
 }
